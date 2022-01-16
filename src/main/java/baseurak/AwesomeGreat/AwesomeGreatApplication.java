3a6324179304b1513518fe9040ffa4b6cb6ae8a5
Ru <dbwls89876@naver.com>
2022-01-16 18:05:43 +0900

    init

---
+++ src/main/java/baseurak/AwesomeGreat/AwesomeGreatApplication.java
@@ -0,0 +1,13 @@
+package baseurak.AwesomeGreat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeGreatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeGreatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/baseurak/AwesomeGreat/AwesomeGreatApplicationTests.java
@@ -0,0 +1,13 @@
+package baseurak.AwesomeGreat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeGreatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
