5f564303d0dbcdb08c31c6cf49d512cd28845b3a
Ru <dbwls89876@naver.com>
2022-05-16 00:48:27 +0900

    security add

--- src/main/java/com/baseurak/AwesomeGreat/user/UserRepository.java
+++ src/main/java/com/baseurak/AwesomeGreat/user/UserRepository.java
@@ -3,10 +3,14 @@ package com.baseurak.AwesomeGreat.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String>
 {
     User findByUserId(String userId);
     User findByUserIdAndPassword(String userId, String password);
     void deleteById(String userId);
+
+    Optional<User> findByEmail(String email);
 }
